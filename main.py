0bb5de2000c7324b129b42eb80f9e84988bcf24d
Shimazaki Takuya <sytstakuya@gmail.com>
2023-05-05 16:33:02 +0900

    100件制限版のコメント付与

--- main.py
+++ main.py
@@ -4,25 +4,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# インテントの設定
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
+# discord.pyの初期化
 client = discord.Client(intents=intents)
 
-# TARGET_CHANNELS = [int(os.getenv("CHANNEL_ID_1")), int(os.getenv("CHANNEL_ID_2"))]
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
+# メッセージ送信イベントをキャッチ
 @client.event
 async def on_message(message):
-    # print(f'message {message}')
+    # 投稿者がボットの場合終了
     if message.author.bot:
         return
 
-    if message.content == "/自己紹介":
+    # 発火条件(/自己紹介と投稿されたとき)
+    if message.content == '/自己紹介':
 
         # サーバーからロールを取得
         male_role = discord.utils.get(message.guild.roles, name='男性')
@@ -32,38 +30,23 @@ async def on_message(message):
 
         # 実行者が「男性」もしくは「女性」ロールを持っているか確認
         if male_role in message.author.roles:
-            channel_id = int(os.environ.get("CHANNEL_ID_MALE"))
-            await message.channel.send("あなたは男性です")
+            channel_id = int(os.environ.get('CHANNEL_ID_MALE'))
         elif female_role in message.author.roles:
-            channel_id = int(os.environ.get("CHANNEL_ID_FEMALE"))
-            await message.channel.send("あなたは女性です")
+            channel_id = int(os.environ.get('CHANNEL_ID_FEMALE'))
         else:
-            await message.channel.send("あなたの性別は不明です")
+            await channel.send('先に性別のロールを取得してください')
             return
 
-        # 環境変数からチャンネルIDを取得
-        channel1_id = int(os.environ.get("CHANNEL_ID_1"))
-        channel2_id = int(os.environ.get("CHANNEL_ID_2"))
-        
-        # メッセージを投稿したチャンネルの取得
-        channel = message.channel
-        
-        # 指定された2つのチャンネルから投稿を検索する
-        for target_channel_id in [channel1_id, channel2_id]:
-            target_channel = client.get_channel(target_channel_id)
-            async for msg in target_channel.history(limit=100):
-                if msg.author == message.author:
-                    # 投稿が見つかった場合、再投稿する
-                    await channel.send(f"{msg.content} (by {msg.author.name})")
-                    return
-        
-        # 投稿が見つからなかった場合、過去の投稿を検索する
-        async for msg in channel.history(limit=100):
+        # 対象の自己紹介チャンネルを取得
+        self_introduction_channel = client.get_channel(channel_id)
+        async for msg in self_introduction_channel.history(limit=100):
             if msg.author == message.author:
-                await channel.send(f"{msg.content} (by {msg.author.name})")
+                # 投稿が見つかった場合、再投稿する
+                await channel.send(f'{msg.content} (by {msg.author.name})')
                 return
 
         # メッセージが見つからなかった場合
-        await channel.send("自己紹介が見つかりませんでした")
+        await channel.send('自己紹介が見つかりませんでした')
 
-client.run(os.getenv("DISCORD_TOKEN"))
+# 実行
+client.run(os.getenv('DISCORD_TOKEN'))
