93c5a1142eb61b3027de8f63ca079ade46da6512
Shimazaki Takuya <sytstakuya@gmail.com>
2023-05-05 12:50:29 +0900

    「/自己紹介」条件を外す

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    if message.content == "/自己紹介":
+    if true:
+    # if message.content == "/自己紹介":
         # 環境変数からチャンネルIDを取得
         channel1_id = int(os.environ.get("CHANNEL_ID_1"))
         channel2_id = int(os.environ.get("CHANNEL_ID_2"))
@@ -27,14 +28,14 @@ async def on_message(message):
         # 指定された2つのチャンネルから投稿を検索する
         for target_channel_id in [channel1_id, channel2_id]:
             target_channel = client.get_channel(target_channel_id)
-            async for msg in target_channel.history(limit=500):
+            async for msg in target_channel.history(limit=100):
                 if msg.author == message.author:
                     # 投稿が見つかった場合、再投稿する
                     await channel.send(f"{msg.content} (by {msg.author.name})")
                     return
         
         # 投稿が見つからなかった場合、過去の投稿を検索する
-        async for msg in channel.history(limit=500):
+        async for msg in channel.history(limit=100):
             if msg.author == message.author:
                 await channel.send(f"{msg.content} (by {msg.author.name})")
                 return
