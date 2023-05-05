729457e260b3d56c6a641079ad339d1d4ec59f43
Shimazaki Takuya <sytstakuya@gmail.com>
2023-05-05 16:03:57 +0900

    ロールの確認を借り実装

--- main.py
+++ main.py
@@ -23,6 +23,24 @@ async def on_message(message):
         return
 
     if message.content == "/自己紹介":
+
+        # サーバーからロールを取得
+        male_role = discord.utils.get(message.guild.roles, name='男性')
+        female_role = discord.utils.get(message.guild.roles, name='男性')
+
+        channel_id = ''
+
+        # 実行者が「男性」もしくは「女性」ロールを持っているか確認
+        if male_role in message.author.roles:
+            channel_id = int(os.environ.get("CHANNEL_ID_MALE"))
+            await message.channel.send("あなたは男性です")
+        elif female_role in message.author.roles:
+            channel_id = int(os.environ.get("CHANNEL_ID_FEMALE"))
+            await message.channel.send("あなたは女性です")
+        else:
+            await message.channel.send("あなたの性別は不明です")
+            return
+
         # 環境変数からチャンネルIDを取得
         channel1_id = int(os.environ.get("CHANNEL_ID_1"))
         channel2_id = int(os.environ.get("CHANNEL_ID_2"))
